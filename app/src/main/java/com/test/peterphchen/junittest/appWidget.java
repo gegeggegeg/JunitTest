9a3f46658fce08f519bb263c3085fd21bdabad19
peter.ph.chen <chenpeter5566@gmail.com>
2018-07-13 11:19:28 +0800

    Add new feature. An appWidget is now available for installing at homeScreen. The text will show random string from database once tapped. This function is for test only.

---
+++ app/src/main/java/com/test/peterphchen/junittest/appWidget.java
@@ -0,0 +1,57 @@
+package com.test.peterphchen.junittest;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.RemoteViews;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class appWidget extends AppWidgetProvider {
+    private ArrayList<String> equations;
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        ComponentName me = new ComponentName(context,appWidget.class);
+        SQLiteDatabase db = new DatabaseHelper(context).getReadableDatabase();
+        String[] projection = {
+                "ROWID AS _id",
+                DatabaseHelper.NUMBER,
+                DatabaseHelper.EQUATION
+        };
+        Cursor cursor = db.query(
+                DatabaseHelper.TABLE,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                DatabaseHelper.NUMBER
+        );
+        equations = new ArrayList<String>();
+        while(cursor.moveToNext()){
+            equations.add(cursor.getString(2));
+        }
+        cursor.close();
+        db.close();
+        RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.appwidget_layout);
+        int limit = equations.size();
+        views.setTextViewText(R.id.widgetText1, equations.get(new Random().nextInt(limit)));
+        views.setTextViewText(R.id.widgetText2,equations.get(new Random().nextInt(limit)));
+        Intent intent = new Intent(context,appWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,appWidgetIds);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context
+                ,0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.widgetText1,pendingIntent);
+        views.setOnClickPendingIntent(R.id.widgetText2,pendingIntent);
+        appWidgetManager.updateAppWidget(me,views);
+    }
+}
